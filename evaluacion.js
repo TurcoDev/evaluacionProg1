ea487d6c36638510740c758dbc8865168c492889
Lucas Salvatori <lucas.o.salvatori@gmail.com>
2025-06-19 20:32:23 -0300

    Descripción de los cambios realizados

---
+++ evaluacion.js
@@ -0,0 +1,17 @@
+/****
+ * Instrucciones para inicializar GIT y GitHub:
+ * 
+ *  1. Crea un repositorio en GitHub.
+ *  2. Inicializar el repo local en una carpeta de tu computadorta:
+ *     git init
+ *  3. Agrega el repositorio remoto de GitHub:
+ *     git remote add origin <URL_DEL_REPOSITORIO>
+ * ------------------------------------------
+ *  4. Creamos un nuevo archivo y realizamos cambios en tu proyecto.
+ *  5. Agrega los cambios al área de preparación (staging area).
+ *     git add .
+ *  6. Realiza un commit de los cambios.
+ *      git commit -m "Descripción de los cambios realizados"
+ *  7. Sube los cambios a GitHub.
+ *     git push -u origin master
+ */
